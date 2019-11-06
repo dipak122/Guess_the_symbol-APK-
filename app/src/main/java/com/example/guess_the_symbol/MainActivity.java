16e63e4c2bf4b15d27d5a2e046571d39f1785c9b
dipak122 <dipak717gautam@gmail.com>
2019-11-06 21:20:33 +0530

    hide_keyboard after submit and if blocks  are blank and press submit will give message

--- app/src/main/java/com/example/guess_the_symbol/MainActivity.java
+++ app/src/main/java/com/example/guess_the_symbol/MainActivity.java
@@ -2,21 +2,42 @@ package com.example.guess_the_symbol;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    EditText edNum1;
-    EditText edNum2;
-    EditText edNum3;
-    EditText edNum4;
-    EditText edNum5;
-    TextView tvAnswer;
-    TextView textView3;
-    TextView show_ans;
+
+public class MainActivity extends AppCompatActivity  {
+    private EditText edNum1;
+    private EditText edNum2;
+    private EditText edNum3;
+    private EditText edNum4;
+    private EditText edNum5;
+    private TextView tvAnswer;
+    private TextView textView3;
+
+    private Button click;
+
+ //   int n1=0,n2,n3,n4,n5;
+//    Thread k = new Thread(new Runnable() {
+//        @Override
+//        public void run() {
+//            if(n1!=0 && n2!=0){
+//                click.setEnabled(true);
+//            }
+//            else{
+//                click.setEnabled(false);
+//            }
+//        }
+//    });
+//    k.start();
+
+
+
 
     public String solve(int a,int b,int c,int d,int res) {
 
@@ -87,6 +108,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -99,12 +123,81 @@ public class MainActivity extends AppCompatActivity {
         edNum5=(EditText) findViewById(R.id.edNum5);
         tvAnswer=(TextView) findViewById(R.id.tvAnswer);
         textView3=(TextView) findViewById(R.id.textView3);
-        show_ans =(TextView) findViewById(R.id.show_ans);
 
-        Button click =(Button) findViewById(R.id.btnSolve);
+
+        click =(Button) findViewById(R.id.btnSolve);
+
+     //   click.setEnabled(false);
+//
+//        if(edNum1.getText().toString().isEmpty()){
+//            click.setEnabled(false);
+//        }
+//        else{
+//            click.setEnabled(true);
+//        }
+
+//        edNum1.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if(!charSequence.toString().isEmpty()){
+//                    n1=12;
+//                }
+//                else{
+//                    n1=0;
+//                }
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+//
+//        edNum2.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if(!charSequence.toString().isEmpty()){
+//                    n2=12;
+//                }
+//                else{
+//                    n2=0;
+//                }
+//
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+//
+//
+//        if(!edNum1.getText().toString().isEmpty() &&  !edNum2.getText().toString().isEmpty()){
+//            click.setEnabled(true);
+//        }
+//        else{
+//            click.setEnabled(false);
+//        }
+
+
+
+
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                try{
                 int n1=Integer.parseInt(edNum1.getText().toString());
                 int n2=Integer.parseInt(edNum2.getText().toString());
                 int n3=Integer.parseInt(edNum3.getText().toString());
@@ -112,13 +205,24 @@ public class MainActivity extends AppCompatActivity {
                 int n5=Integer.parseInt(edNum5.getText().toString());
 
 
+
                 String ans= solve(n1,n2,n3,n4,n5);
                 String result="Symbol :- \n "+ans;
                // show_ans.setText(ans);
 
                 tvAnswer.setText(result);
+                try{close();}catch(Exception e){}}
+                catch (Exception e){
+                    tvAnswer.setText("Fill the remaining blocks");
+                    try{close();}catch(Exception e1){}
+                }
 
 
+
+            }
+            private void close(){
+                InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
 
             }
         });
